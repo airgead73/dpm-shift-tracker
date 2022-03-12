f5fd493036de4e9fc891595d8252a7bd6e8f5989
Dan Moneypenny <catullus1973@gmail.com>
2022-03-12 00:40:30 -0800

    add base

---
+++ index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const app = require('./app/app.js')
+const port = process.env.PORT || 6060;
+
+if (port === undefined) {
+  throw new Error(
+    "Port is not defined."
+  );
+}
+
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  switch (error.code) {
+    case 'EACCES':
+      console.log(`Port ${port} requires privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.log(`Port ${port} is already in use.`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+async function openBrowser(port) {
+  const open = require('open');
+  await open(`http://localhost:${port}`, { app: { name: 'google chrome'} } )
+}
+
+function onListening() {
+  if(process.env.NODE_ENV === "development") openBrowser(port);
+  console.log(`Listening on ${port}.`)
+}
