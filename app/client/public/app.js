2ca5626b32fcdd7179a9fba35b8c172f40a25444
Dan Moneypenny <catullus1973@gmail.com>
2022-03-16 00:53:10 -0700

    update

--- app/client/public/app.js
+++ app/client/public/app.js
@@ -1,6 +1,53 @@
-import initForms from './modules/forms/initForms';
-
 const forms = Array.from(document.querySelectorAll('.form'));
 
-if(forms.length) initForms(forms);
+const fetchData = async ($form) => {
+  const attrs = $form.getAttributeNames().reduce((acc, name) => {
+    return {...acc, [name]: $form.getAttribute(name)}
+  }, {});
 
+  const options = {
+    method: attrs.method,
+    headers: {
+      'Content-Type': attrs.enctype,
+      'Accept': attrs.enctype
+    }
+  }
+
+  const needsBody = attrs.method === 'POST' || attrs.method === 'PUT';
+ 
+  if(needsBody) {
+    let body = {};
+    let formElements = Array.from($form.elements);
+    formElements = formElements.filter(element => {
+      if(element.hasAttribute('name')) {
+        return element;
+      }
+    });
+
+    formElements.forEach(element => {
+      const property = element.getAttribute('name');
+      const value = $form[property].value;
+      body[property] = value;
+      
+    });
+    
+    options.body = JSON.stringify(body);
+    
+  }
+
+  const response = await fetch(attrs.action, options);
+  const data = await response.json();
+  const { success } = data;
+
+  if(success) window.location.reload();
+  
+
+}
+
+forms.forEach(form => {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    fetchData(event.target);
+  });
+  
+});
--- app/client/public/modules/forms/initForms.js
+++ app/client/public/modules/forms/initForms.js
@@ -1,14 +1,2 @@
-import fetchData from './fetchData';
 
-const initForms = ($forms) => {
-
-  $forms.forEach(form => {
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      fetchData(event.target);
-    });
-    
-  });
-
-}
 
