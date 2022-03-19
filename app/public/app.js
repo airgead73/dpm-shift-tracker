4801072b4bac25bfaf3281a61f81a6f076b526b0
Dan Moneypenny <catullus1973@gmail.com>
2022-03-18 22:20:50 -0700

    add forms

---
+++ app/public/app.js
@@ -0,0 +1,8 @@
+const forms = Array.from(document.querySelectorAll('.form'));
+import { initForms } from './modules/index.js';
+
+if(forms.length) {
+  initForms(forms)
+} else {
+  console.log('page does not have forms')
+}
