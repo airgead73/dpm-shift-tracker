fad74473597f83222ba1c7344de52e9d3bfe568c
Dan Moneypenny <catullus1973@gmail.com>
2022-03-26 01:14:13 -0700

    update

--- app/units-client/landing.controller.js
+++ app/units-client/landing.controller.js
@@ -10,17 +10,11 @@ const { getActive } = require('./shift.services');
  exports.landing = asyncHandler(async (req, res, next) => {
 
   const activeShift = await getActive();
-  const renderPath = activeShift === null ? 'pages/shifts-add' : 'pages/shifts-update';
-  const mainClass = activeShift === null ? 'main--add' : 'main--update';
 
-  return res
-    .status(200)
-    .render(renderPath, {
-      success: true,
-      title: "DPM shift tracker",
-      landing: true,
-      main: mainClass,
-      shift: activeShift
-    });
+  if(activeShift === null) {
+    return res.status(200).redirect('/shifts/add')
+  } else {
+    return res.status(200).redirect(`shifts/add/${activeShift.id}/update?update=items`)
+  }
     
 });
