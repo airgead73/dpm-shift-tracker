55261761c11c20f5031d09046deeddc49a464b54
Dan Moneypenny <catullus1973@gmail.com>
2022-04-02 09:28:59 -0700

    fix gulp dev

--- gulpfile.js/development/index.js
+++ gulpfile.js/development/index.js
@@ -1,10 +1,3 @@
-const { series } = require('gulp');
-const scss = require('./scss');
-const { jsConcat, jsBrowserify, jsBabel, jsClean } = require('./js');
-const clean = require('./clean');
 const watchTasks = require('./watch');
 
-exports.scss = scss;
-exports.js = series(jsConcat, jsBrowserify, jsBabel, jsClean);
-exports.clean = clean;
 exports.dev = watchTasks;
